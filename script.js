8ffa992ef6dfeb0aa1471a9ed25c09a6be4a0c99
Joann Kim <jsk031@gmail.com>
2015-07-23 18:36:53 -0400

    Updated .js file

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var main = function() {
-  
+  $(".day").click(function(){
+    $(this).next().toggle();
+
+    $(this).find(".glyphicon.glyphicon-plus").toggleClass("glyphicon-minus");
+  });
 };
+
 
 $(document).ready(main);
